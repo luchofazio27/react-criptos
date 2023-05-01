3120923ecba4ec1950ebee1688410e66b662103d
luchofazio27 <luchofazio@gmail.com>
2023-04-30 22:50:36 -0300

    Finalizacion de proyecto criptomonedas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Formulario from "./components/Formulario";
 import ImagenCripto from "./img/imagen-criptos.png";
+import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -44,17 +46,21 @@ function App() {
 
   const [monedas, setMonedas] = useState({});
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0) {
       
       const cotizarCripto = async () => {
+        setCargando(true)
         const { moneda, criptomoneda} = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+        setCargando(false)
 
       }
 
@@ -69,6 +75,9 @@ function App() {
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
         <Formulario setMonedas={setMonedas} />
+
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado}/>}
       </div>
     </Contenedor>
   );
